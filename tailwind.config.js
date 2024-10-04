2860916262f04a11bcfdb52e773ef98deb653842
Dan Kim <itsdanya@gmail.com>
2024-10-05 01:39:25 +0300

    Update tailwind config content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/views/**/*.turbo_stream.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
