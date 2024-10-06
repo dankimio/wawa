81c3b7ca2e3bc1dbc4fc6a8d7f4e06e1c2628f72
Dan Kim <itsdanya@gmail.com>
2024-10-07 02:48:44 +0300

    Reset form on submit

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,7 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="reset-form"
 export default class extends Controller {
+  static targets = ["input"];
+
   connect() {
+    console.log("connected");
+  }
+
+  reset() {
+    this.element.reset();
+
+    this.inputTarget.focus();
   }
 }
