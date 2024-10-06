46ac9474e3a2cd0f3d8a4a3794e79b1d2899edf8
Dan Kim <itsdanya@gmail.com>
2024-10-07 00:48:09 +0300

    Join rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,11 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: %i[show join leave]
+
   def index
     @rooms = Room.all
   end
 
   def show
-    @room = Room.find(params[:id])
   end
 
   def new
@@ -21,7 +22,19 @@ class RoomsController < ApplicationController
     end
   end
 
+  def join
+    @room.users << Current.user unless @room.users.include?(Current.user)
+    redirect_to @room
+  end
+
+  def leave
+  end
+
   private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
 
   def room_params
     params.require(:room).permit(:name)
