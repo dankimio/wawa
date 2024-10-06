0c49fc0aa227729b570fe81ed91ac5c505f05047
Dan Kim <itsdanya@gmail.com>
2024-10-06 19:44:39 +0300

    Add rooms#new

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,24 @@ class RoomsController < ApplicationController
   def index
     @rooms = []
   end
+
+  def new
+    @room = {}
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      redirect_to @room
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   resources :users, only: %i[create]
-  resources :rooms, only: %i[index]
+  resources :rooms, only: %i[index new create show]
 end
