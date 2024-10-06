c294580bf9a5fcdbbb8aa5c8fdfd511d5adb730a
Dan Kim <itsdanya@gmail.com>
2024-10-07 00:19:27 +0300

    Migrate rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_04_220635) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_06_211851) do
+  create_table "rooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -29,5 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_10_04_220635) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "rooms", "users"
   add_foreign_key "sessions", "users"
 end
