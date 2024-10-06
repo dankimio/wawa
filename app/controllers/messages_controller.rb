af7b90112671b724935babf3ab228d57b851acc7
Dan Kim <itsdanya@gmail.com>
2024-10-07 02:06:15 +0300

    Add message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,24 @@
 class MessagesController < ApplicationController
+  before_action :set_room
+
+  def create
+    message = @room.messages.build(message_params.merge(user: Current.user))
+    message.save!
+
+    # if message.save
+    #   redirect_to @room, notice: "Message sent!"
+    # else
+    #   redirect_to @room, alert: "Message not sent!"
+    # end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
 end
