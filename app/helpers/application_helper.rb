39f7f8a0922160f3c0d1eae272e4f6194b44489d
Dan Kim <itsdanya@gmail.com>
2024-10-07 02:33:06 +0300

    Style flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def class_for_flash_type(type)
+    case type
+    when "notice" then "bg-blue-50 text-blue-400 border-2 border-blue-100"
+    when "alert" then "bg-red-50 text-red-400 border-2 border-red-100"
+    end
+  end
 end
