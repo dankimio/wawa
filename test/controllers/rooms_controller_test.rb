5a1bd6f7d7e4cc7832afac4b7f3b973511e54d26
Dan Kim <itsdanya@gmail.com>
2024-10-07 04:01:05 +0300

    Test rooms controller

--- test/controllers/rooms_controller_test.rb
+++ test/controllers/rooms_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class RoomsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @room = rooms(:one)
+    sign_in_user
+  end
+
+  test "should get index" do
+    get rooms_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_room_url
+    assert_response :success
+  end
+
+  test "should create room" do
+    assert_difference("Room.count") do
+      post rooms_url, params: { room: { name: @room.name } }
+    end
+
+    assert_redirected_to room_url(Room.last)
+  end
+
+  test "should show room" do
+    get room_url(@room)
+    assert_response :success
+  end
 end
