ea4e774b7e93c3e1d9a0341dbd9dfb1b1e516a6d
Dan Kim <itsdanya@gmail.com>
2024-10-07 04:04:12 +0300

    Test users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get new" do
+    get signup_url
+    assert_response :success
+  end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post users_url, params: { user: { email: "new@email.com", name: @user.name, password: "password" } }
+    end
+
+    assert_redirected_to root_url
+  end
 end
