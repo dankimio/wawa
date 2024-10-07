690ea8437c105cdbda00ec51c56fac82fc466c66
Dan Kim <itsdanya@gmail.com>
2024-10-07 03:54:46 +0300

    Test room user

--- test/models/room_user_test.rb
+++ test/models/room_user_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class RoomUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @room_user = room_users(:one)
+  end
+
+  test "should be valid" do
+    assert @room_user.valid?
+  end
+
+  test "should be unique" do
+    duplicate_room_user = @room_user.dup
+    @room_user.save
+    assert_not duplicate_room_user.valid?
+  end
 end
